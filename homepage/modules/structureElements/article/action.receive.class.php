1df57f1c248e204997d736529c636022084398fb
aw-dmitri <dmitri@artweb.ee>
2019-04-16 17:44:21 +0300

    minor phpdoc

--- homepage/modules/structureElements/article/action.receive.class.php
+++ homepage/modules/structureElements/article/action.receive.class.php
@@ -4,10 +4,15 @@ class receiveArticle extends structureElementAction
 {
     protected $loggable = true;
 
+    /**
+     * @param structureManager $structureManager
+     * @param controller $controller
+     * @param articleElement $structureElement
+     * @return mixed|void
+     */
     public function execute(&$structureManager, &$controller, &$structureElement)
     {
         if ($this->validated) {
-            $structureElement->prepareActualData();
             if ($structureElement->getDataChunk('image')->originalName !== null) {
                 $structureElement->image = $structureElement->id;
                 $structureElement->originalName = $structureElement->getDataChunk('image')->originalName;
