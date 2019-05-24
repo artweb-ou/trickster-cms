c79a10a3b4d2d69828281a7dc0dd3e8cec90d744
aw-dmitri <dmitri@artweb.ee>
2019-05-24 16:08:10 +0300

    Fix for chrome

--- homepage/js/public/component.FeedbackForm.js
+++ homepage/js/public/component.FeedbackForm.js
@@ -1,10 +1,6 @@
 window.FeedbackFormComponent = function(componentElement) {
-  var buttonText = '';
   var fileInputElement;
   var dropAreaElement;
-  var controlElement;
-  var dropArea;
-  var button;
 
   var init = function() {
     new AjaxFormComponent(componentElement, tracking.feedbackTracking);
@@ -43,48 +39,5 @@ window.FeedbackFormComponent = function(componentElement) {
     fileInputElement.files = files;
     eventsManager.fireEvent(fileInputElement, 'change');
   };
-  // var createDom = function() {
-  // 	var container = document.createElement('div');
-  // 	var button = document.createElement('div');
-  // 	var span = document.createElement('div');
-  // 	span.className = 'feedback_attach_file_text';
-  // 	span.innerText = 'Attach file';
-  // 	button.className = 'feedback_attach_file';
-  // 	container.className = 'feedback_attach_file_container';
-  // 	container.appendChild(button);
-  // 	container.appendChild(span);
-  // 	controlElement.insertBefore(container, controlElement.firstChild);
-  // 	createDropArea();
-  // };
-  // var createDropArea = function() {
-  // 	dropArea = document.createElement('div');
-  // 	dropArea.className = 'feedback_form_droparea';
-  // 	dropArea.style.visibility = 'hidden';
-  // 	dropArea.style.position = 'absolute';
-  // 	dropArea.style.top = 0;
-  // 	dropArea.style.bottom = 0;
-  // 	dropArea.style.left = 0;
-  // 	dropArea.style.right = 0;
-  // 	var logo = document.createElement('div');
-  // 	logo.className = 'droparea_logo';
-  // 	var span = document.createElement('span');
-  // 	span.innerText = 'Drop your file to attach';
-  // 	dropArea.appendChild(logo);
-  // 	dropArea.appendChild(span);
-  // 	dropAreaElement.parentElement.appendChild(dropArea);
-  // 	dropArea.ondrop = function(event) {
-  // 		dropAreaDropHandler(event);
-  // 	}
-  // };
-  // var processInputElement = function() {
-  // 	inputElement.style.position = 'absolute';
-  // 	inputElement.style.visibility = 'hidden';
-  // 	inputElement.style.left = 0;
-  // 	inputElement.style.top = 0;
-  // };
-  // var clickHandler = function() {
-  // 	inputElement.click();
-  // };
-
   init();
 };
