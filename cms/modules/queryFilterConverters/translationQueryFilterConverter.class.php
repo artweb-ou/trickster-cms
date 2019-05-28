a011900c3746b3409c6b18a7d4d5102f17a2b1a4
aw-dmitri <dmitri@artweb.ee>
2019-05-28 19:13:38 +0300

    fixed double translations in results

--- cms/modules/queryFilterConverters/translationQueryFilterConverter.class.php
+++ cms/modules/queryFilterConverters/translationQueryFilterConverter.class.php
@@ -2,5 +2,27 @@
 
 class translationQueryFilterConverter extends queryFilterConverter
 {
-    use SimpleQueryFilterConverterTrait;
+    public function convert($sourceData, $sourceType)
+    {
+        /**
+         * @var \Illuminate\Database\Connection $db ;
+         */
+        $db = $this->getService('db');
+        $query = $db
+            ->table($this->getTableName())
+            ->select('id')
+            ->whereIn('id', function($query){
+                $query
+                    ->from('structure_elements')
+                    ->where('structureType', '=', 'translation')
+                    ->select('id');
+            })
+        ;
+        return $query;
+    }
+
+    protected function getTableName()
+    {
+        return 'module_translation';
+    }
 }
