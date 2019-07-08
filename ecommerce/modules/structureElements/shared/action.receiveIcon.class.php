bf82c3c9ff0cb4777fa4fc512a765e5b3694c788
Ivan Krotov <ivan.krotov@artweb.ee>
2019-07-08 13:50:16 +0300

    link/ unlink fixes

--- ecommerce/modules/structureElements/shared/action.receiveIcon.class.php
+++ ecommerce/modules/structureElements/shared/action.receiveIcon.class.php
@@ -16,10 +16,25 @@ class receiveIconShared extends structureElementAction
             /**
              * @var $linksManager linksManager
              */
+            $linkType = 'genericIcon'.ucfirst($structureElement->structureType);
             $linksManager = $this->getService('linksManager');
-            $conectedIconsList = $linksManager->getConnectedIdList($parentId, 'genericIconProduct');
+            $icons = $structureElement->connectedIconIds;
+            $connectedGenericIconsList = $linksManager->getConnectedIdIndex($structureElement->id, $linkType);
+            foreach ($structureElement->connectedIconIds as $connectedIcon) {
+                if (!isset($connectedGenericIconsList[(int)$connectedIcon])) {
+                    $linksManager->linkElements($connectedIcon, $structureElement->id, $linkType);
+                } else {
+                    unset ($connectedGenericIconsList[$connectedIcon]);
+                }
+            }
+            if(!empty($connectedGenericIconsList)) {
+                foreach ($connectedGenericIconsList as $genericIcon => $value) {
+                    $linksManager->unLinkElements($structureElement->id, $genericIcon, $linkType);
+                }
+            }
+            $url = $structureElement->getUrl();
         }
-        $structureElement->executeAction('showIconForm');
+        $controller->redirect($url.'id:'.$structureElement->id.'/action:showIconForm');
     }
 
     public function setExpectedFields(&$expectedFields)
