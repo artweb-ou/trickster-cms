e0967748c7558862168c3a4514ef04c1a67548e2
aw-dmitri <dmitri@artweb.ee>
2019-07-16 18:02:31 +0300

    Events calendar now shows parent if its role is not set to "container"

--- homepage/modules/structureElements/eventsList/action.show.class.php
+++ homepage/modules/structureElements/eventsList/action.show.class.php
@@ -5,6 +5,11 @@ class showEventsList extends structureElementAction
     public function execute(&$structureManager, &$controller, &$structureElement)
     {
         $structureElement->setViewName("show");
+        if ($structureElement->final && $structureElement->role !== 'container') {
+            if ($parent = $structureManager->getElementsFirstParent($structureElement->id)) {
+                $controller->restart($parent->URL);
+            }
+        }
     }
 }
 
