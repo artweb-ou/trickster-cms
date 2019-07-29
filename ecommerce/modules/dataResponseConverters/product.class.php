e0924d6064fe587c738ab143af8f374768faea3d
aw-zhannanurumova <zhanna.nurumova@artweb.ee>
2019-07-29 14:43:57 +0300

    ajax Full ProductList

--- ecommerce/modules/dataResponseConverters/product.class.php
+++ ecommerce/modules/dataResponseConverters/product.class.php
@@ -7,27 +7,68 @@ class productDataResponseConverter extends StructuredDataResponseConverter
     protected function getRelationStructure()
     {
         return [
-            'id' => 'id',
-            'title' => 'title',
-            'searchTitle' => 'getSearchTitle',
-            'url' => 'getUrl',
-            'structureType' => 'structureType',
-            'image' => 'image',
-            'content' => 'content',
-            'introduction' => 'introduction',
-            'dateCreated' => function ($element) {
+            'id'                        => 'id',
+            'title'                     => 'title',
+            'searchTitle'               => 'getSearchTitle',
+            'url'                       => 'getUrl',
+            'structureType'             => 'structureType',
+            'image'                     => 'image',
+            'content'                   => 'content',
+
+            // new
+            'code'                      => 'code',
+            'languageId'                => 'languageId',
+            'price'                     => 'price',
+            'oldPrice'                  => 'oldPrice',
+            'showincategory'            => 'showincategory',
+            'brandId'                   => 'brandId',
+            'brandTitle'                => function ($element) {
+                if ($brandElement = $element->getBrandElement()) {
+                    return $brandElement->title;
+                }
+                return '';
+            },
+            'purchaseCount'             => 'purchaseCount',
+            'inactive'                  => 'inactive',
+            'color'                     => 'color',
+            'deliveryStatus'            => 'deliveryStatus',
+            'availabilityStatus'        => 'availabilityStatus',
+            'importCode'                => 'importCode',
+            'productPriceMultiplier'    => 'productPriceMultiplier',
+            'productPriceAddition'      => 'productPriceAddition',
+            'deliveryPriceType'         => 'deliveryPriceType',
+            'importOrigin'              => 'importOrigin',
+            'importId'                  => 'importId',
+            'metaTitle'                 => 'metaTitle',
+            'metaDescription'           => 'metaDescription',
+            'availability'              => 'availability',
+            'quantity'                  => 'quantity',
+            'canonicalUrl'              => 'canonicalUrl',
+            'metaDenyIndex'             => 'metaDenyIndex',
+            'minimumOrder'              => 'minimumOrder',
+            'lastPurchaseDate'          => 'lastPurchaseDate',
+            'importPrice'               => 'importPrice',
+            'h1'                        => 'h1',
+            'qtFromConnectedCategories' => 'qtFromConnectedCategories',
+
+            // new END
+
+
+            'introduction'     => 'introduction',
+            'dateCreated'      => function ($element) {
                 return $element->getValue('dateCreated');
             },
-            'dateModified' => function ($element) {
+            'dateModified'     => function ($element) {
                 return $element->getValue('dateModified');
             },
             'introductionText' => function ($element, $scope) {
                 return $scope->htmlToPlainText($element->introduction);
             },
-            'contentText' => function ($element, $scope) {
+            'contentText'      => function ($element, $scope) {
                 return $scope->htmlToPlainText($element->content);
             },
         ];
+
     }
 
     protected function getPresetsStructure()
@@ -49,6 +90,50 @@ class productDataResponseConverter extends StructuredDataResponseConverter
                 'url',
                 'structureType',
                 'introductionText',
+            ],
+            'detailed'    => [
+                'id',
+                'title',
+                'dateCreated',
+                'dateModified',
+                'url',
+                'image',
+                'content',
+                'introduction',
+
+                // new
+                'code',
+                'languageId',
+                'price',
+                'oldPrice',
+                'showincategory',
+                'brandId',
+                'brandTitle',
+                'purchaseCount',
+                'inactive',
+                'color',
+                'deliveryStatus',
+                'availabilityStatus',
+                'importCode',
+                'productPriceMultiplier',
+                'productPriceAddition',
+                'deliveryPriceType',
+                'importOrigin',
+                'importId',
+                'metaTitle',
+                'metaDescription',
+                'availability',
+                'quantity',
+                'canonicalUrl',
+                'metaDenyIndex',
+                'minimumOrder',
+                'lastPurchaseDate',
+                'importPrice',
+                'h1',
+                'qtFromConnectedCategories',
+                // new END
+
+
             ],
         ];
     }
---
+++ homepage/modules/applications/ajaxProductList.class.php
@@ -0,0 +1,84 @@
+<?php
+
+class ajaxProductListApplication extends controllerApplication
+{
+    public $rendererName = 'json';
+    protected $applicationName = 'ajaxProductList';
+    protected $mode = 'public';
+
+    public function initialize()
+    {
+        $controller = controller::getInstance();
+        if ($controller->getParameter('mode')) {
+            $mode = $controller->getParameter('mode');
+            if ($mode == 'admin') {
+                $this->mode = 'admin';
+            } else {
+                $this->mode = 'public';
+            }
+        }
+        $configManager = $controller->getConfigManager();
+        if ($this->mode == 'admin') {
+            $this->startSession($this->mode, $configManager->get('main.adminSessionLifeTime'));
+        } else {
+            $this->startSession($this->mode, $configManager->get('main.publicSessionLifeTime'));
+        }
+
+        $this->createRenderer();
+    }
+
+    public function execute($controller)
+    {
+        /**
+         * @var Cache $cache
+         */
+        //        $cache = $this->getService('Cache');
+        //        $cache->enable();
+
+        $response = new ajaxResponse();
+        $languagesManager = $this->getService('languagesManager');
+
+        $response->setPreset('detailed');
+        /**
+         * @var structureManager $structureManager
+         */
+
+        $structureManager = $this->getService('structureManager', [
+            'rootUrl'       => $controller->rootURL,
+            'rootMarker'    => $this->getService('ConfigManager')->get('main.rootMarkerPublic'),
+            'configActions' => false,
+        ], true);
+        $structureManager->setRequestedPath([$languagesManager->getCurrentLanguageCode()]);
+
+        $productsValue = [];
+        if ($elementId = $controller->getParameter('elementId')) {
+            if ($element = $structureManager->getElementById($elementId)) {
+//                if ($controller->getParameter('limit')) {
+//                    
+//                }
+                $products = $element->getProductsList();
+            }
+        }
+
+        //        $products = $structureManager->getProductsByCategory($category);
+        //
+                            $response->setResponseData("product", $products);
+
+        $status = 'success';
+        $this->renderer->assign('responseStatus', $status);
+        $this->renderer->assign('responseData', $response->responseData);
+
+        $this->renderer->setCacheControl('no-cache');
+        $this->renderer->display();
+    }
+
+    public function getUrlName()
+    {
+        if ($this->mode == 'admin') {
+            return 'admin';
+        } else {
+            return '';
+        }
+    }
+}
+
