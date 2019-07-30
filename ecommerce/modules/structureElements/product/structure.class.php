d6a4457ca522bb2f7b29ca940a5889978490e98f
aw-dmitri <dmitri@artweb.ee>
2019-07-30 13:38:53 +0300

    Product's H1 SEO template fix

--- ecommerce/modules/structureElements/product/structure.class.php
+++ ecommerce/modules/structureElements/product/structure.class.php
@@ -440,7 +440,7 @@ class productElement extends structureElement implements
 
             //brand and parameters should be loaded only if the element is final
             if ($this->final) {
-                if ($categoryMetaDescriptionTemplate = $this->getCategoryMetaDescriptionTemplate()) {
+                if ($categoryMetaDescriptionTemplate = $this->getFirstDataFromParents('metaDescriptionTemplate')) {
                     $this->textContent = $this->populateSeoTemplate($categoryMetaDescriptionTemplate);
                 } else {
                     if ($brandElement = $this->getBrandElement()) {
@@ -1535,25 +1535,19 @@ class productElement extends structureElement implements
         return $this->productUnit;
     }
 
-    private function getCategoryMetaDescriptionTemplate()
-    {
-        return $this->getFirstDataFromParents('metaDescriptionTemplate');
-    }
-
-    private function getCategoryMetaTitleTemplate()
-    {
-        return $this->getFirstDataFromParents('metaTitleTemplate');
-    }
-
-    private function getCategoryMetaH1Template()
-    {
-        return $this->getFirstDataFromParents('metaH1Template');
-    }
 
     public function getTemplatedMetaTitle()
     {
-        if ($categoryMetaTitleTemplate = $this->getCategoryMetaTitleTemplate()) {
+        if ($categoryMetaTitleTemplate = $this->getFirstDataFromParents('metaTitleTemplate')) {
             return $this->populateSeoTemplate($categoryMetaTitleTemplate);
+        }
+        return '';
+    }
+
+    public function getTemplatedH1()
+    {
+        if ($categoryH1Template = $this->getFirstDataFromParents('metaH1Template')) {
+            return $this->populateSeoTemplate($categoryH1Template);
         }
         return '';
     }
@@ -1959,7 +1953,7 @@ class productElement extends structureElement implements
     {
         if ($currentAction == 'showImages') {
             $this->allowedTypes = ['galleryImage'];
-        } elseif($currentAction == 'showTexts'){
+        } elseif ($currentAction == 'showTexts') {
             $this->allowedTypes = ['subArticle'];
         } else {
             $this->allowedTypes = [];
@@ -1967,7 +1961,8 @@ class productElement extends structureElement implements
         return $this->allowedTypes;
     }
 
-    public function getNewElementAction() {
+    public function getNewElementAction()
+    {
         return 'showForm';
     }
 
