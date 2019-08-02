004e9bb2a7ea00272b407722d06baf26455651c1
aw-dmitri <dmitri@artweb.ee>
2019-08-02 17:45:58 +0300

    Shared translations for shared form

--- cms/modules/structureElements/shared/form.sharedForm.php
+++ cms/modules/structureElements/shared/form.sharedForm.php
@@ -5,14 +5,17 @@ class SharedFormStructure extends ElementForm
     protected $structure = [
         'title'        => [
             'type' => 'input.multi_language_text',
+            'translationsGroup' => 'shared',
         ],
         'marker'       => [
             'type' => 'input.text',
+            'translationsGroup' => 'shared',
         ],
         'displayMenus' => [
             'type'      => 'select.universal_options_multiple',
             'method'    => 'getDisplayMenusInfo',
             'condition' => 'checkDisplayMenus',
+            'translationsGroup' => 'shared',
         ]
     ];
 
