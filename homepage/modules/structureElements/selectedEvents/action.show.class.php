3d94ad917976400b53618295a6b722bc44040365
aw-zhannanurumova <zhanna.nurumova@artweb.ee>
2019-08-05 04:44:33 +0300

    SelectedEvents FormStructure, add go-to-link, commented code remove

--- homepage/modules/structureElements/selectedEvents/action.show.class.php
+++ homepage/modules/structureElements/selectedEvents/action.show.class.php
@@ -4,37 +4,7 @@ class showSelectedEvents extends structureElementAction
 {
     public function execute(&$structureManager, &$controller, &$structureElement)
     {
-/*        if ($fixedElement = $structureElement->getFixedElement()) {
-            if ($structureElement->title == '') {
-                $structureElement->title = $fixedElement->title;
-            }
-
-            $structureElement->URL = $fixedElement->URL;
-        }*/
-//        $structureElement->setViewName($structureElement->getCurrentLayout());
-//        $structureElement->linkItems = $structureManager->getElementsChildren($structureElement->id);
         $structureElement->setTemplate('selectedEvents.content.tpl');
-
-
-/*        if ($fixedElement = $structureElement->getFixedElement()) {
-            if ($structureElement->title == '') {
-                $structureElement->title = $fixedElement->title;
-            }
-            if ($structureElement->link == '') {
-                $structureElement->link = $fixedElement->URL;
-            }
-            if ($structureElement->originalName == '' && $fixedElement->originalName) {
-                $structureElement->originalName = $fixedElement->originalName;
-                $structureElement->image = $fixedElement->image;
-            }
-            if ($structureElement->content == '' && $fixedElement->introduction) {
-                $structureElement->content = $fixedElement->introduction;
-            }
-            if ($structureElement->content == '' && $fixedElement->content) {
-                $structureElement->content = $fixedElement->content;
-            }
-        }*/
-
     }
 }
 
