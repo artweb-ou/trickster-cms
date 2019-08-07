2442ba36d9bec45938d2d6ed8b1fbf8fcf0e5ca4
aw-dmitri <dmitri@artweb.ee>
2019-08-07 13:47:52 +0300

    event calendars selector change

--- homepage/js/admin/component.eventForm.js
+++ homepage/js/admin/component.eventForm.js
@@ -2,9 +2,9 @@ window.EventFormComponent = function(componentElement) {
     var ajaxSelectElement;
 
     var init = function() {
-        if (ajaxSelectElement = _('select.event_connected_eventslists_select', componentElement)[0]) {
-            new AjaxSelectComponent(ajaxSelectElement, 'eventsList', 'admin');
-        }
+        // if (ajaxSelectElement = _('select.event_connected_eventslists_select', componentElement)[0]) {
+        //     new AjaxSelectComponent(ajaxSelectElement, 'eventsList', 'admin');
+        // }
     };
 
     init();
