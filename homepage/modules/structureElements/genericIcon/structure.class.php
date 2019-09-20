9c73b8308a9b54975d6a6703b80bcb5010f67e2a
ivan.krotov <ivan.krotov@artweb.ee>
2019-09-20 13:52:57 +0300

    fixes

--- homepage/modules/structureElements/genericIcon/structure.class.php
+++ homepage/modules/structureElements/genericIcon/structure.class.php
@@ -52,6 +52,7 @@ class genericIconElement extends structureElement implements ImageUrlProviderInt
         $moduleStructure['applicableToAllProducts'] = 'checkbox';
         $moduleStructure['iconProductAvail'] = 'serializedIndex';
         $moduleStructure['iconProductParameters'] = 'numbersArray';
+        $moduleStructure['selectedIcons'] = 'numbersArray';
 
     }
 
