1726c2aa72d9292d5989a12c04748e4246ecd3bc
moroz1999 <moroz1999@gmail.com>
2021-11-02 21:58:21 +0200

    CORS

--- homepage/modules/applications/ajax.class.php
+++ homepage/modules/applications/ajax.class.php
@@ -13,41 +13,55 @@ class ajaxApplication extends controllerApplication
 
     public function execute($controller)
     {
-        /**
-         * @var Cache $cache
-         */
-        $cache = $this->getService('Cache');
-        $cache->enable();
-
-        $currentElement = false;
-        $this->renderer->assign('responseData', []);
-
-        if ($controller->getParameter('id')) {
-            //todo: replace with $controller->rootURL and test.
-            $structureManager = $this->getService('structureManager', [
-                'rootUrl' => $controller->baseURL,
-                'rootMarker' => $this->getService('ConfigManager')->get('main.rootMarkerPublic'),
-            ], true);
-
-            $this->processRequestParameters();
-
-            $languagesManager = $this->getService('LanguagesManager');
-            $elementId = $controller->getParameter('id');
-
-            if (is_numeric($elementId)) {
-                $currentElement = $structureManager->getElementById($elementId, $languagesManager->getCurrentLanguageId());
-            } else {
-                $currentElement = $structureManager->getCurrentElement();
-            }
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
         }
-        if ($currentElement) {
-            if ($this->renderer->getAttribute('responseStatus') === false) {
-                $this->renderer->assign('responseStatus', 'success');
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
             }
-            $this->renderer->display();
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            }
         } else {
-            $this->renderer->assign('responseStatus', 'fail');
-            $this->renderer->fileNotFound();
+            /**
+             * @var Cache $cache
+             */
+            $cache = $this->getService('Cache');
+            $cache->enable();
+
+            $currentElement = false;
+            $this->renderer->assign('responseData', []);
+
+            if ($controller->getParameter('id')) {
+                //todo: replace with $controller->rootURL and test.
+                $structureManager = $this->getService('structureManager', [
+                    'rootUrl' => $controller->baseURL,
+                    'rootMarker' => $this->getService('ConfigManager')->get('main.rootMarkerPublic'),
+                ], true);
+
+                $this->processRequestParameters();
+
+                $languagesManager = $this->getService('LanguagesManager');
+                $elementId = $controller->getParameter('id');
+
+                if (is_numeric($elementId)) {
+                    $currentElement = $structureManager->getElementById($elementId, $languagesManager->getCurrentLanguageId());
+                } else {
+                    $currentElement = $structureManager->getCurrentElement();
+                }
+            }
+            if ($currentElement) {
+                if ($this->renderer->getAttribute('responseStatus') === false) {
+                    $this->renderer->assign('responseStatus', 'success');
+                }
+                $this->renderer->display();
+            } else {
+                $this->renderer->assign('responseStatus', 'fail');
+                $this->renderer->fileNotFound();
+            }
         }
     }
 
